47e428a34cac3039b53d18944835a730835ed233
DaVinci0003 <133808076+DaVinci0003@users.noreply.github.com>
2024-05-23 19:59:31 +0200

    saving commit

--- Hotel-Project-Backend/src/main/java/com/mcorp/hotelprojectbackend/config/WebConfig.java
+++ Hotel-Project-Backend/src/main/java/com/mcorp/hotelprojectbackend/config/WebConfig.java
@@ -1,11 +1,8 @@
-package com.mcorp.hotelprojectbackend.congigurations;
+package com.mcorp.hotelprojectbackend.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
